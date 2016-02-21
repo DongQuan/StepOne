0af09c2fad7f04c0efff1f805878aa9515bcb4b9
Jxuan95 <jiangxuan9001@gmail.com>
2016-02-21 21:20:49 +0800

    快速排序

--- java/jxuan95/java/Main.java
+++ java/jxuan95/java/Main.java
@@ -8,10 +8,14 @@ public class Main {
 
     public static void main(String[] args){
 
+        int[] a = new int[]{49, 38, 65, 97, 76, 13, 27, 49, 78, 34, 12, 64, 5,
+                4, 62, 99, 98, 54, 56, 17, 18, 23, 34, 15, 35, 25, 53, 51};
+
         Arithmetic arithmetic = new Arithmetic();
 //        arithmetic.insertSort();
 //        arithmetic.selectSort();
 //        arithmetic.bubbleSort();
-        arithmetic.shellSort();
+//        arithmetic.shellSort();
+        arithmetic.quickSort();
     }
 }
