539e993833c28c905a637a01b6280084d82bf337
Jxuan95 <jiangxuan9001@gmail.com>
2016-03-11 21:08:20 +0800

    二维数组的查找

--- 算法及数据结构/jxuan95/java/JianZhiOffer.java
+++ 算法及数据结构/jxuan95/java/JianZhiOffer.java
@@ -11,7 +11,19 @@ public class JianZhiOffer {
      * 在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
      * 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
      */
-    public boolean Find(int [][] array,int target) {
-
+    public boolean Find(int[][] array, int target) {
+        int row = array.length;
+        int list = array[0].length;
+        int i = 0;
+        while (i < row) {
+            for (int j = 0; j < list; j++) {
+                if(array[i][j]==target){
+                    System.out.println(array[i][j]);
+                    return true;
+                }
+            }
+            i++;
+        }
+        return false;
     }
 }
