f2fd041a9656df3372b1f310e0dd6c310deaf24f
Jxuan95 <jiangxuan9001@gmail.com>
2016-03-11 21:01:50 +0800

    牛客网剑指offer

---
+++ 算法及数据结构/jxuan95/java/JianZhiOffer.java
@@ -0,0 +1,17 @@
+package jxuan95.java;
+
+/**
+ * Created by jxuan on 16-3-11.
+ * 牛客网剑指offer习题
+ */
+public class JianZhiOffer {
+
+    /**
+     * 二维数组中的查找
+     * 在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+     * 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+     */
+    public boolean Find(int [][] array,int target) {
+
+    }
+}
