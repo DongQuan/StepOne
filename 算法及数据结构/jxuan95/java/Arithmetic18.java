7e74e70cc5865af2644b8f4b2c47c57ae47236df
Jxuan95 <jiangxuan9001@gmail.com>
2016-03-30 09:46:34 +0800

    算法：数列求和

---
+++ 算法及数据结构/jxuan95/java/Arithmetic18.java
@@ -0,0 +1,26 @@
+package jxuan95.java;
+
+import java.util.Scanner;
+
+/**
+ * Created by jxuan on 16-3-30.
+ * 数列的定义如下：
+ * 数列的第一项为n，以后各项为前一项的平方根，求数列的前m项的和。
+ */
+public class Arithmetic18 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            int n = in.nextInt();
+            int m = in.nextInt();
+            double temp = Math.sqrt(n);
+            double sum = n;
+            for (int i = 1; i < m; i++) {
+                sum += temp;
+                temp = Math.sqrt(temp);
+            }
+            System.out.printf("%.2f", sum);
+            System.out.println();
+        }
+    }
+}
