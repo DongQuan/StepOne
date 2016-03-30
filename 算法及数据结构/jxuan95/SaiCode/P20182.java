06338bd2df16c384e19f0ee497731a7abb8c4cfc
Jxuan95 <jiangxuan9001@gmail.com>
2016-03-30 15:24:48 +0800

    母牛生小牛解法2

---
+++ 算法及数据结构/jxuan95/SaiCode/P20182.java
@@ -0,0 +1,25 @@
+package jxuan95.SaiCode;
+
+import java.util.Scanner;
+
+/**
+ * Created by jxuan on 16-3-30.
+ * 母牛生小牛问题2
+ */
+public class P20182 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] a = new int[56];
+        a[1] = 1;
+        a[2] = 2;
+        a[3] = 3;
+        a[4] = 4;
+        for (int i = 4; i < 56; i++) {
+            a[i] = a[i - 1] + a[i - 3];
+        }
+        while (in.hasNext()) {
+            int n = in.nextInt();
+            System.out.println(a[n]);
+        }
+    }
+}
