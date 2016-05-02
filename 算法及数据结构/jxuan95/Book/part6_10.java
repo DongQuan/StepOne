fc63b832f5dbe5592a7ec25a42cd4d839bb3ac3e
Jxuan95 <jiangxuan9001@gmail.com>
2016-05-02 15:34:36 +0800

    10,11

---
+++ 算法及数据结构/jxuan95/Book/part6_10.java
@@ -0,0 +1,20 @@
+package jxuan95.Book;
+
+import java.util.Scanner;
+
+/**
+ * Created by jxuan on 16-5-2.
+ * 数位重组
+ */
+public class part6_10 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            int n = in.nextInt();//第一个数字
+            int m = in.nextInt();//第二个数字
+
+
+
+        }
+    }
+}
