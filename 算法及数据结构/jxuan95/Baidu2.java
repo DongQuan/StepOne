2a51ce32351d2481f79f9146b023843d093650a8
Jxuan <xuanmail95@163.com>
2016-05-08 00:48:23 +0800

    修改

--- 算法及数据结构/jxuan95/Baidu2.java
+++ 算法及数据结构/jxuan95/Baidu2.java
@@ -1,7 +1,5 @@
 package jxuan95;
-
 import java.util.Scanner;
-
 /**
  * Created by jxuan on 16-4-21.
  * 百度前端笔试第二题
--- 算法及数据结构/jxuan95/Chain.java
+++ 算法及数据结构/jxuan95/Chain.java
@@ -4,5 +4,15 @@ package jxuan95;
  * Created by jxuan on 16-4-19.
  */
 public class Chain {
+    public static void main(String[] args){
+        int countx = 0;
+        int x = 9999;
+        while(x>0){
+            countx++;
+            x=x&(x-1);
+        }
+        System.out.println(countx);
+    }
+
 
 }
